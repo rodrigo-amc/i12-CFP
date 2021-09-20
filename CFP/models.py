f3c7714626223742f4791a8231bda153132c49f3
rodo <rodrigo.amc1@gmail.com>
2021-09-20 14:50:11 -0300

    Creo la aplicaicon CFP para getionar los centros de formacion y sus cursos. Instalo PILLOW para poder manejar imagenes

---
+++ CFP/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import DO_NOTHING
+from django.db.models.fields import CharField
+from django.core.validators import MinValueValidator
+# Create your models here.
+
+# region Utiles
+class Localidad(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class diaHora(models.Model):
+    opcionDias = [
+        ('L', 'Lunes'),
+        ('Ma', 'Martes'),
+        ('Mi', 'Miercoles'),
+        ('J', 'Jueves'),
+        ('V', 'Viernes'),
+        ('S', 'Sabado'),
+    ]
+    dia = models.CharField(max_length=9, choices=opcionDias)
+    hora = models.TimeField()
+#endregion utiles
+
+
+class CentroDeFormacion(models.Model):
+    nombre = CharField(max_length=20)
+    localidad = models.ForeignKey(Localidad, on_delete=DO_NOTHING)
+    domicilio = models.CharField(max_length=150)
+    email = models.EmailField()
+
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=200)
+    cenForm = models.ForeignKey(CentroDeFormacion, on_delete=DO_NOTHING)
+    inscAbierta = models.BooleanField(default=False)
+    habilitado = models.BooleanField(default=False)
+    practico = models.BooleanField(default=True)
+    cantHoras = models.PositiveIntegerField(validators=[MinValueValidator(4)])
+    fechaInicio = models.DateField()
+    fechaFin = models.DateField()
+    diasHorarios = models.ManyToManyField(diaHora)
--- Usuarios/models.py
+++ Usuarios/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 #from Carreras.models import Carrera
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.db.models.deletion import DO_NOTHING
+from CFP.models import Localidad, Curso
 # Create your models here.
 """
 Por ahora solo voy a crear Alumno y Profesor
@@ -20,50 +21,34 @@ class appUser(AbstractUser):
 
 
 class Alumno(models.Model):
-    usr_alumno = models.OneToOneField(appUser, on_delete=models.CASCADE, primary_key=True)
-    
-    dni = models.PositiveIntegerField(unique=True, validators=[MinValueValidator(20000000), MaxValueValidator(99999999)])
     #nombre = es el campo "first_name" de AbstractUser
     #apellido = es el campo "last_name" de AbstractUser
+    usr_alumno = models.OneToOneField(appUser, on_delete=models.CASCADE, primary_key=True)
+
+    dni = models.PositiveIntegerField(unique=True, validators=[MinValueValidator(20000000), MaxValueValidator(99999999)])
     
-
-    """ opcion_sexo = [
-        ('f', 'Femenino'),
-        ('m', 'Masculino')
-    ]
-    sexo = models.CharField(max_length=9, choices=opcion_sexo)
-
-    opcion_casadao = [
-        ('c', 'casado'),
-        ('s', 'soltero')
-    ]
-    est_civil = models.CharField(max_length=7, choices=opcion_casadao)
-    hijos = models.PositiveIntegerField(default=0, blank=True, null=True) """
     fecha_nacimiento = models.DateField()
+
+    dniImg = models.ImageField()
+    autorizacion = models.ImageField()
 
     telefono = models.CharField(max_length=20)
     #email= es el campo "email" de AbstractUser
     #OJO edité "AbstractUser" (/django/contrib/auth/models.py) para que el campo email sea único
     domicilio = models.CharField(max_length=100)
 
-    #Esto deberia relacionar a tablas pero por ahora lo dejo asi
-    localidad = models.CharField(max_length=100)
-    partido = models.CharField(max_length=100)
+    localidad = models.ForeignKey(Localidad, on_delete=DO_NOTHING)
 
-    #ForeingKey es una relacion de 1 a N
-    #En este caso 1 Carrera Tiene N Alumnos. 1 Alumno tiene solo 1 Carrera 
-#    carrera = models.ForeignKey(Carrera, on_delete=models.DO_NOTHING)
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.usr_alumno.username
 
 
 
-
-
 class Profesor(models.Model):
     usr_profesor = models.OneToOneField(appUser, on_delete=models.CASCADE, primary_key=True)
-
+    telefono = models.CharField(max_length=20)
     #nombre = es el campo "first_name" de AbstractUser
     #apellido = es el campo "last_name" de AbstractUser
     #email= es el campo "email" de AbstractUser
